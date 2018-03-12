6b5c493d3cdee0560f9ded02000c33c8610f241e
Wassim Sabra <wassim_sabra@hotmail.com>
2018-03-12 12:41:43 -0400

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var http = require('http');
+var express = require('express');
+var app = express();
+
+var fs = require('fs');
+function getFilesList(f, dir){
+    var files = [];
+    fs.readdirSync(dir).forEach(
+        file => {
+            if(fs.lstatSync(dir+file).isFile()){
+                files.push(file);
+            }
+        }
+    );    
+    return JSON.stringify(files);
+}
+
+app.get('/', function(req, res){
+    res.send('{ "files": '+getFilesList(fs, './public/')+ '}');
+});
+
+app.get('/:name', function(req, res){
+    fs.open('./public/'+req.params.name, 'r', function(err, file){
+        if(!err){
+            fs.readFile(file, {encoding: 'utf-8'}, function (err, data){
+                res.send(req.headers.accept);
+            });
+        }
+    });
+});
+
+app.post('/:file', function(req, res){
+    console.log(req.body);
+});
+
+app.listen(3000, function(){
+    console.log('Listening at port 3000');
+});
