6224d8e83dc51eee4094d87cf29bb42f9f623e3c
Wassim Sabra <wassim_sabra@hotmail.com>
2018-03-21 00:06:24 -0400

    Concurrency

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var net = require('net');
 var fs = require('fs');
 var path = require('path');
 var server = net.createServer();
+var lock = {};
 
 server.listen(9999, '127.0.0.1');//Listen on IP, PORT
 
@@ -26,9 +27,18 @@ function handle(socket) {
     //Received Data from the socket
     socket.on('data', (data) => {
         var request = data.toString().trim();
+        console.log(request);
         //Process the request
         parseRequest(request, function (options) {
-            if (options['path'].match(/\//g).length > 1) {//Trying to access a directory not allowed /public/
+            if(lock[options.path] !== "none" && lock[options.path] !== undefined){
+                var temp = `<h1>Somebody is ${lock[options.path]} to the file requested. Please try again later</h1>`;
+                socket.write(`${options['http']} 423 OK\r\n`);
+                socket.write('Content-Type: text/html\r\n');
+                socket.write(`Content-Length: ${temp.length}\r\n\r\n`);//Two CR LF End of headers, the following is the body 
+                socket.write(temp);
+                socket.end();
+            }
+            else if (options['path'].match(/\//g).length > 1) {//Trying to access a directory not allowed /public/
                 socket.write(`${options['http']} 404 OK\r\n`);
                 socket.write('Content-Type: text/html\r\n');
                 socket.write(`Content-Length: 19\r\n\r\n`);//Two CR LF End of headers, the following is the body 
@@ -51,6 +61,7 @@ function handle(socket) {
                         }
                         fs.open('./public' + options.path, 'r', function (err, file) {
                             if (!err) {
+                                lock[options.path] = "Writing";
                                 fs.writeFile('./public/' + options.path, body, function (err) {
                                     if (err) {
                                         socket.write(`${options['http']} 500 OK\r\n`);
@@ -67,9 +78,11 @@ function handle(socket) {
                                         socket.end();
                                     }
                                 });
+                                lock[options.path] = "none";
                             }
                             else {
                                 fs.writeFile('./public' + options.path, body, function (err) {
+                                    lock[options.path] = "Writing";
                                     if (err) {
                                         socket.write(`${options['http']} 500 OK\r\n`);
                                         socket.write('Content-Type: text/html\r\n');
@@ -84,12 +97,14 @@ function handle(socket) {
                                         socket.write('<h1>New File Created</h1>');
                                         socket.end();
                                     }
+                                    
+                                    lock[options.path] = "none";
                                 });
                             }
                         });
                     }
                     else {
-                        socket.write(`${options['http']} 400 OK\r\n`);
+                        socket.write(`${options['http']} 411 OK\r\n`);
                         socket.write('Content-Type: text/html\r\n');
                         socket.write(`Content-Length: 37\r\n\r\n`);//Two CR LF End of headers, the following is the body 
                         socket.write('<h1>Bad request. Unknown Content</h1>');
@@ -108,8 +123,10 @@ function handle(socket) {
                     socket.end();
                 }
                 else {
+                    //Try to open file for reading
                     fs.open('./public' + options.path, 'r', function (err, file) {
                         if (!err) {//Return the content of the file if exists
+                            lock[options.path] = "Reading";
                             fs.readFile(file, { encoding: 'utf-8' }, function (err, data) {
                                 socket.write(`${options['http']} 200 OK\r\n`);
                                 socket.write(`Content-Type: ${options['accept']}\r\n`);
@@ -118,6 +135,7 @@ function handle(socket) {
                                 socket.write(`Content-Length: ${data.length}\r\n\r\n`);//Two CR LF End of headers, the following is the body 
                                 socket.write(`${data}`);
                                 socket.end();
+                                //lock[options.path] = "none";
                             });
                         }
                         else {//File not found
@@ -147,7 +165,7 @@ function handle(socket) {
         var options = {};
         options['method'] = firstLine[0].toLowerCase();
         options['path'] = firstLine[1].toLowerCase();
-        options['http'] = firstLine[2].toLowerCase();
+        options['http'] = firstLine[2];
 
         var temp;
         for (var i = 1; i < arr.length; i++) {
